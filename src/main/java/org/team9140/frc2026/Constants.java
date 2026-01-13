f908a7263a4145da8d9c071206dfdbd3c5148ca4
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2026-01-13 17:44:02 -0600

    Add basic vendordeps and subsystems

---
+++ src/main/java/org/team9140/frc2026/Constants.java
@@ -0,0 +1,11 @@
+package org.team9140.frc2026;
+
+public class Constants {
+    public static class Cantdle {}
+
+    public static class Climber {}
+
+    public static class Intake {}
+
+    public static class Shooter {}
+}
---
+++ src/main/java/org/team9140/frc2026/subsystems/Intake.java
@@ -0,0 +1,7 @@
+package org.team9140.frc2026.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+}
