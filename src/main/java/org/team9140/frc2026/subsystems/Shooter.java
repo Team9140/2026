efa6dd899766492de7a3b9d31d653dd10e451ac0
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-15 17:36:44 -0600

    Added constructor and instance methods to turret and shooter

---
+++ src/main/java/org/team9140/frc2026/subsystems/Shooter.java
@@ -0,0 +1,19 @@
+package org.team9140.frc2026.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase{
+    private final TalonFX motor;
+
+    private Shooter instance;
+
+    public Shooter getInstance() {
+        return (instance == null) ? instance = new Shooter() : instance;
+    }
+
+    private Shooter() {
+        motor = new TalonFX(0);
+    }
+}
--- src/main/java/org/team9140/frc2026/subsystems/Turret.java
+++ src/main/java/org/team9140/frc2026/subsystems/Turret.java
@@ -1,7 +1,19 @@
 package org.team9140.frc2026.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Turret extends SubsystemBase {
+    private final TalonFX motor;
 
+    private Turret instance;
+
+    public Turret getInstance() {
+        return (instance == null) ? instance = new Turret() : instance;
+    }
+
+    private Turret() {
+        motor = new TalonFX(0);
+    }
 }
