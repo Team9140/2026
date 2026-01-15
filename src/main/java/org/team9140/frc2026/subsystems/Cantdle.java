66d8353a2317099d19062b589d6e0f5dab19c42a
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-15 17:02:11 -0600

    removed unused import Color8bit

--- src/main/java/org/team9140/frc2026/subsystems/Cantdle.java
+++ src/main/java/org/team9140/frc2026/subsystems/Cantdle.java
@@ -3,7 +3,6 @@ package org.team9140.frc2026.subsystems;
 import java.util.Optional;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.team9140.lib.Util;
