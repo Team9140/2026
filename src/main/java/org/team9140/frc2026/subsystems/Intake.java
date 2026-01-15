3219ed2481560beefd35d0530b9f8bd907ccd2e4
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-15 17:40:18 -0600

    Added constructor and instance method to Intake

--- src/main/java/org/team9140/frc2026/subsystems/Intake.java
+++ src/main/java/org/team9140/frc2026/subsystems/Intake.java
@@ -1,7 +1,17 @@
 package org.team9140.frc2026.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Intake extends SubsystemBase {
+    private final TalonFX motor;
+    private static Intake instance;
 
+    private Intake(){
+        motor = new TalonFX(0);
+    }
+
+    public static Intake getInstance(){
+        return (instance == null) ? instance = new Intake() : instance;
+    }
 }
