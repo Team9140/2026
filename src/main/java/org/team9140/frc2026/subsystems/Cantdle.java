dfe7ccebfab96974e87fe6654ad51e3401c7c288
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-15 16:59:14 -0600

    Updated candle class for new pheonix api

--- src/main/java/org/team9140/frc2026/subsystems/Cantdle.java
+++ src/main/java/org/team9140/frc2026/subsystems/Cantdle.java
@@ -1,7 +1,60 @@
 package org.team9140.frc2026.subsystems;
 
+import java.util.Optional;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.team9140.lib.Util;
+
+import com.ctre.phoenix6.controls.SolidColor;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.RGBWColor;
 
 public class Cantdle extends SubsystemBase {
+    private static Cantdle instance;
+
+    public static final RGBWColor RED = new RGBWColor(255, 0, 0);
+    public static final RGBWColor GREEN = new RGBWColor(0, 255, 0);
+    public static final RGBWColor BLUE = new RGBWColor(0, 0, 255);
+    public static final RGBWColor PINK = new RGBWColor(245, 110, 229);
+    public static final RGBWColor ORANGE = new RGBWColor(255, 157, 0);
+    public static final RGBWColor PURPLE = new RGBWColor(227, 18, 254);
+    public static final RGBWColor OFF = new RGBWColor(0, 0, 0);
+    public RGBWColor current = new RGBWColor(0, 0, 0);
+
+    private CANdle candle;
+
+    private SolidColor colorcontrol;
+
+    private Cantdle() {
+        this.candle = new CANdle(0);
+        colorcontrol = new SolidColor(0, 0);
+
+        if (Optional.of(DriverStation.Alliance.Red).equals(Util.getAlliance()))
+            this.setColor(RED);
+        else
+            this.setColor(BLUE);
+    }
+
+    public static Cantdle getInstance() {
+        return (instance == null) ? instance = new Cantdle() : instance;
+    }
+
+    public Command setColor(RGBWColor color) {
+        return this.runOnce(() -> {
+            this.candle.setControl(colorcontrol.withColor(color));
+            this.current = color;
+        });
+    }
+
+    public RGBWColor getAllianceColor() {
+        if (Optional.of(DriverStation.Alliance.Blue).equals(Util.getAlliance())) {
+            return BLUE;
+        } else {
+            return RED;
+        }
+    }
 
 }
