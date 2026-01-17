c42d463f0756b6f7ac5d231e4fd823e122244d47
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-17 11:34:07 -0600

    move yaw and pitch motor commands

--- src/main/java/org/team9140/frc2026/subsystems/Turret.java
+++ src/main/java/org/team9140/frc2026/subsystems/Turret.java
@@ -1,6 +1,7 @@
 package org.team9140.frc2026.subsystems;
 
 import org.team9140.frc2026.Constants;
+import org.team9140.lib.Util;
 
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
@@ -8,7 +9,10 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class Turret extends SubsystemBase {
     private final TalonFX yawMotor = new TalonFX(0);
@@ -69,7 +73,25 @@ public class Turret extends SubsystemBase {
         pitchMotor.setControl(pitchMM.withPosition(0));
     }
 
+    public Command moveYawToPosition(double pos) {
+        return this.runOnce(() -> {
+            this.yawTargetPosition = pos;
+            yawMotor.setControl(yawMM.withPosition(yawTargetPosition / 2.0 / Math.PI));
+        }).andThen(new WaitUntilCommand(yawIsAtPosition));
+    }
 
+    public Command movePitchToPosition(double pos) {
+        return this.runOnce(() -> {
+            this.pitchTargetPosition = pos;
+            pitchMotor.setControl(pitchMM.withPosition(pitchTargetPosition / 2.0 / Math.PI));
+        }).andThen(new WaitUntilCommand(pitchIsAtPosition));
+    }
 
+    public final Trigger yawIsAtPosition = new Trigger(() -> 
+        Util.epsilonEquals(this.yawMotor.getPosition(false).getValueAsDouble(), this.yawTargetPosition)
+    );
 
+    public final Trigger pitchIsAtPosition = new Trigger(() -> 
+        Util.epsilonEquals(this.pitchMotor.getPosition(false).getValueAsDouble(), this.pitchTargetPosition)
+    );  
 }
