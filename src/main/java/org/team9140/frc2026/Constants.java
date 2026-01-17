da5341b402bc0b1e1a9f1e718749298150648f0b
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-17 12:50:26 -0600

    Constant for hoop position

--- src/main/java/org/team9140/frc2026/Constants.java
+++ src/main/java/org/team9140/frc2026/Constants.java
@@ -1,5 +1,8 @@
 package org.team9140.frc2026;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class Constants {
     public static class Ports {
         public static final String CANIVORE_NAME = "buzz"; // Buzz Aldrin
@@ -33,5 +36,7 @@ public class Constants {
         public static final double PITCH_KP = 150.0;
         public static final double PITCH_KI = 0;
         public static final double PITCH_KD = 9.6;
+
+        public static final Pose2d HOOP_POSITION = new Pose2d(4.62534, 4.03479, new Rotation2d());
     }
 }
