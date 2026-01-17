9087dcb0e0feb9d5021b7e44d7c312ab7193dd49
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-17 11:12:13 -0600

    Added motion magic to two motors in turret

--- src/main/java/org/team9140/frc2026/subsystems/Turret.java
+++ src/main/java/org/team9140/frc2026/subsystems/Turret.java
@@ -1,11 +1,24 @@
 package org.team9140.frc2026.subsystems;
 
+import org.team9140.frc2026.Constants;
+
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Turret extends SubsystemBase {
-    private final TalonFX motor;
+    private final TalonFX yawMotor = new TalonFX(0);
+    private final TalonFX pitchMotor = new TalonFX(0);
+
+    private double yawTargetPosition;
+    private double pitchTargetPosition;
+
+    private MotionMagicTorqueCurrentFOC yawMM = new MotionMagicTorqueCurrentFOC(0);
+    private MotionMagicTorqueCurrentFOC pitchMM= new MotionMagicTorqueCurrentFOC(0);
 
     private static Turret instance;
 
@@ -14,6 +27,49 @@ public class Turret extends SubsystemBase {
     }
 
     private Turret() {
-        this.motor = new TalonFX(0);
+        MotionMagicConfigs yawMMConfigs = new MotionMagicConfigs()
+            .withMotionMagicAcceleration(Constants.Turret.YAW_ACCELERATION)
+            .withMotionMagicCruiseVelocity(Constants.Turret.YAW_CRUISE_VELOCITY);
+
+        Slot0Configs yawSlot0Configs = new Slot0Configs()
+            .withKS(Constants.Turret.YAW_KS)
+            .withKV(Constants.Turret.YAW_KV)
+            .withKA(Constants.Turret.YAW_KA)
+            .withKP(Constants.Turret.YAW_KP)
+            .withKI(Constants.Turret.YAW_KI)
+            .withKD(Constants.Turret.YAW_KD);
+
+        MotionMagicConfigs pitchMMConfigs = new MotionMagicConfigs()
+            .withMotionMagicAcceleration(Constants.Turret.PITCH_ACCELERATION)
+            .withMotionMagicCruiseVelocity(Constants.Turret.PITCH_CRUISE_VELOCITY);
+
+        Slot0Configs pitchSlot0Configs = new Slot0Configs()
+            .withKS(Constants.Turret.PITCH_KS)
+            .withKV(Constants.Turret.PITCH_KV)
+            .withKA(Constants.Turret.PITCH_KA)
+            .withKP(Constants.Turret.PITCH_KP)
+            .withKI(Constants.Turret.PITCH_KI)
+            .withKD(Constants.Turret.PITCH_KD);
+
+        TalonFXConfiguration yawConfig = new TalonFXConfiguration()
+            .withMotionMagic(yawMMConfigs)
+            .withSlot0(yawSlot0Configs);
+
+        TalonFXConfiguration pitchConfig = new TalonFXConfiguration()
+            .withMotionMagic(pitchMMConfigs)
+            .withSlot0(pitchSlot0Configs);
+
+        yawMM = new MotionMagicTorqueCurrentFOC(0).withSlot(0);
+        pitchMM = new MotionMagicTorqueCurrentFOC(0).withSlot(0);
+
+        yawMotor.getConfigurator().apply(yawConfig);
+        pitchMotor.getConfigurator().apply(pitchConfig);
+
+        yawMotor.setControl(yawMM.withPosition(0));
+        pitchMotor.setControl(pitchMM.withPosition(0));
     }
+
+
+
+
 }
