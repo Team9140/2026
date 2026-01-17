11c6254313e7549ca85f4ca603861bf1a980b2a8
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-17 11:10:06 -0600

    Added test constants to turret

--- src/main/java/org/team9140/frc2026/Constants.java
+++ src/main/java/org/team9140/frc2026/Constants.java
@@ -13,5 +13,25 @@ public class Constants {
 
     public static class Shooter {}
 
-    public static class Turret {}
+    public static class Turret {
+        public static final double YAW_ACCELERATION = 1;
+        public static final double YAW_CRUISE_VELOCITY = 1;
+
+        public static final double YAW_KS = 0;
+        public static final double YAW_KV = 0;
+        public static final double YAW_KA = 0;
+        public static final double YAW_KP = 150.0;
+        public static final double YAW_KI = 0;
+        public static final double YAW_KD = 9.6;
+
+        public static final double PITCH_ACCELERATION = 1;
+        public static final double PITCH_CRUISE_VELOCITY = 1;
+
+        public static final double PITCH_KS = 0;
+        public static final double PITCH_KV = 0;
+        public static final double PITCH_KA = 0;
+        public static final double PITCH_KP = 150.0;
+        public static final double PITCH_KI = 0;
+        public static final double PITCH_KD = 9.6;
+    }
 }
