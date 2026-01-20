3bab7cff41f4c071c90b96a4cef68b96d6f13799
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2026-01-20 17:35:00 -0600

    add TunerConstants.java and Ports in Constants.java

--- src/main/java/org/team9140/frc2026/Constants.java
+++ src/main/java/org/team9140/frc2026/Constants.java
@@ -1,6 +1,10 @@
 package org.team9140.frc2026;
 
 public class Constants {
+    public static class Ports {
+        public static final String CANIVORE_NAME = "buzz"; // Buzz Aldrin
+    }
+
     public static class Cantdle {}
 
     public static class Climber {}
