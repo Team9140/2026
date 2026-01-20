9559de0915238abf5fda8dd241ad2dc9b6cb12b4
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-20 16:59:38 -0600

    Added simulation using single jointed arm model

--- src/main/java/org/team9140/frc2026/subsystems/Turret.java
+++ src/main/java/org/team9140/frc2026/subsystems/Turret.java
@@ -8,9 +8,13 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
@@ -25,6 +29,30 @@ public class Turret extends SubsystemBase {
 
     private MotionMagicTorqueCurrentFOC yawMM = new MotionMagicTorqueCurrentFOC(0);
     private MotionMagicTorqueCurrentFOC pitchMM= new MotionMagicTorqueCurrentFOC(0);
+
+    private TalonFXSimState yawMotorSimState;
+    private final SingleJointedArmSim yawMotorSim = new SingleJointedArmSim(
+        DCMotor.getKrakenX60Foc(1),
+        60, 
+        1, 
+        0.2, 
+        0, 
+        Math.PI, 
+        false, 
+        0
+    );
+
+    private TalonFXSimState pitchMotorSimState;
+    private final SingleJointedArmSim pitchMotorSim = new SingleJointedArmSim(
+        DCMotor.getKrakenX60Foc(1),
+        60, 
+        1, 
+        0.2, 
+        0, 
+        Math.PI, 
+        false, 
+        0
+    );
 
     private static Turret instance;
 
@@ -106,8 +134,28 @@ public class Turret extends SubsystemBase {
     );  
 
     @Override
-    public void simulationPeriodic() {
+    public void periodic() {
+        yawMotorSimState = yawMotor.getSimState();
+        double yawSimVolts = yawMotorSimState.getMotorVoltage();
+        yawMotorSim.setInputVoltage(yawSimVolts);
+        yawMotorSim.update(0.02);
 
+        pitchMotorSimState = pitchMotor.getSimState();
+        double pitchSimVolts = pitchMotorSimState.getMotorVoltage();
+        pitchMotorSim.setInputVoltage(pitchSimVolts);
+        pitchMotorSim.update(0.02);
+
+        SmartDashboard.putNumber("yawAngle", yawMotor.getPosition().getValueAsDouble());
+        yawMotor.getPosition().refresh();
+
+        SmartDashboard.putNumber("pitchAngle", pitchMotor.getPosition().getValueAsDouble());
+        pitchMotor.getPosition().refresh();
+
+        yawMotorSimState.setRawRotorPosition(yawMotorSim.getAngleRads() / 2.0 / Math.PI);
+        yawMotorSimState.setRotorVelocity(yawMotorSim.getVelocityRadPerSec() / 2.0 / Math.PI);
+
+        pitchMotorSimState.setRawRotorPosition(pitchMotorSim.getAngleRads() / 2.0 / Math.PI);
+        pitchMotorSimState.setRotorVelocity(pitchMotorSim.getVelocityRadPerSec() / 2.0 / Math.PI);
     }
 
 }
