0b062da12b042b8e63f4622e76c2e97ad10309ec
Gijs Landwehr <gijslandwehr@gmail.com>
2026-01-24 14:31:41 -0600

    copy bits from 2025 for extension control

--- src/main/java/org/team9140/frc2026/subsystems/Intake.java
+++ src/main/java/org/team9140/frc2026/subsystems/Intake.java
@@ -3,10 +3,14 @@ package org.team9140.frc2026.subsystems;
 import com.ctre.phoenix6.configs.*;
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.signals.InvertedValue;
+
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 import org.team9140.frc2026.Constants;
 
 public class Intake extends SubsystemBase {
@@ -57,6 +61,21 @@ public class Intake extends SubsystemBase {
         return (instance == null) ? instance = new Intake() : instance;
     }
 
+    public Distance getPosition(boolean refresh) {
+        return Constants.Elevator.SPOOL_CIRCUMFERENCE
+                .times(this.rightMotor.getPosition(false).getValue().in(Rotations));
+    }
+
+    public Command setPosition(Distance position) {
+        return this.runOnce(() -> {
+            this.targetPosition = position;
+            this.extendMotor.setControl(this.motionMagic.withPosition(0));
+        }).andThen(new WaitUntilCommand(atPosition));
+    }
+
+    public final Trigger atPosition = new Trigger(() -> this.getPosition(true).isNear(this.targetPosition, Constants.Elevator.POSITION_epsilon));
+
+    
     //convertion of motor rotations to length extended
     public Command armIn(){
         return this.runOnce(() -> {
