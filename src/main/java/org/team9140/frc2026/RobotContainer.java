e140ad20ef886f3320db347b7c91406402a35c53
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-30 17:52:17 -0600

    Added methods to drivetrain and added moving capacbilities in RobotContainer
    Co-authored-by: Abhinav Devarakonda <abhi3231@users.noreply.github.com>

--- src/main/java/org/team9140/frc2026/RobotContainer.java
+++ src/main/java/org/team9140/frc2026/RobotContainer.java
@@ -4,15 +4,30 @@
 
 package org.team9140.frc2026;
 
+import org.team9140.frc2026.generated.TunerConstants;
+import org.team9140.frc2026.subsystems.CommandSwerveDrivetrain;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
+
+  private final CommandXboxController controller = new CommandXboxController(0);
+  private final CommandSwerveDrivetrain drivetrain = TunerConstants.getDrivetrain();
+  private final SwerveTelemetry logger = new SwerveTelemetry(Constants.Drive.MAX_TELEOP_VELOCITY);
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    drivetrain.setDefaultCommand(
+        drivetrain.teleopDrive(controller::getLeftX, controller::getLeftY,
+            controller::getRightX).ignoringDisable(true));
+    
+    this.drivetrain.registerTelemetry(logger::telemeterize);
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
