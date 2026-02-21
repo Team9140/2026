823d9f9d397b45e2e61bd2d5d9cda7fc18ecde53
ShauryaVatsal <vatshaurya@gmail.com>
2026-02-21 15:26:22 -0600

    added new constants

--- src/main/java/org/team9140/frc2026/Constants.java
+++ src/main/java/org/team9140/frc2026/Constants.java
@@ -33,7 +33,22 @@ public class Constants {
 
         public static double MIN_TELEOP_VELOCITY = 0.05;
         public static double MIN_TELEOP_ROTATION = Math.toRadians(2);
-        
+        public static double MIN_AUTON_VELOCITY = 0.05;
+        public static double MIN_AUTON_ROTATION = Math.toRadians(2);
+
+        public static final double X_CONTROLLER_P = 2.5 * 3.141592653589793238462643383279502884197169399375;
+        public static final double X_CONTROLLER_I = 0.0;
+        public static final double X_CONTROLLER_D = 0.015; // TODO: Raise value
+        public static final double Y_CONTROLLER_P = X_CONTROLLER_P;
+        public static final double Y_CONTROLLER_I = X_CONTROLLER_I;
+        public static final double Y_CONTROLLER_D = X_CONTROLLER_D;
+        public static final double HEADING_CONTROLLER_P = 8.0; // 8.0
+        public static final double HEADING_CONTROLLER_I = 0.0;
+        public static final double HEADING_CONTROLLER_D = 0.03; // 0.04
+
+
+        public static final double REACHEDPOSE_DEBOUNCE = 0.5;
+
     }
 
     public static class Shooter {
