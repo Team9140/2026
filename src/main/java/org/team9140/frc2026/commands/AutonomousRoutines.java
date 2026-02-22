f8e068e176a5a68204c529d4e077d1ad6aa72936
ShauryaVatsal <vatshaurya@gmail.com>
2026-02-21 18:58:13 -0600

    Added shoot preiload command for some secodns

--- src/main/java/org/team9140/frc2026/commands/AutonomousRoutines.java
+++ src/main/java/org/team9140/frc2026/commands/AutonomousRoutines.java
@@ -1,9 +1,15 @@
 package org.team9140.frc2026.commands;
 
+import org.team9140.frc2026.helpers.AimAlign;
 import org.team9140.frc2026.subsystems.Climber;
 import org.team9140.frc2026.subsystems.CommandSwerveDrivetrain;
 import org.team9140.frc2026.subsystems.Intake;
 import org.team9140.frc2026.subsystems.Shooter;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class AutonomousRoutines {
     CommandSwerveDrivetrain drivetrain;
@@ -15,4 +21,19 @@ public class AutonomousRoutines {
     public AutonomousRoutines(CommandSwerveDrivetrain drivetrain) {
         this.drivetrain = drivetrain;
     }    
+
+    public Command shootPreload(double seconds) {
+        Pose2d currPos = drivetrain.getState().Pose;
+        Translation2d goalPos = AimAlign.getZone(currPos).getTranslation();
+        return shooter.setYawAngle(AimAlign.yawAngleToPos(
+            currPos, 
+            goalPos
+            )).andThen(shooter.setSpeed(AimAlign.getRequiredSpeed(
+                currPos, 
+                goalPos, 
+                drivetrain.getState().Speeds
+            ))).andThen(new WaitCommand(seconds)
+            ).andThen(shooter.off());
+    }
+    
 }
