840ea8918cf82701fc07255012a1223bcbd123bd
Gijs Landwehr <gijslandwehr@gmail.com>
2026-02-26 17:36:03 -0600

    controller bindings

--- src/main/java/org/team9140/frc2026/RobotContainer.java
+++ src/main/java/org/team9140/frc2026/RobotContainer.java
@@ -5,16 +5,23 @@
 package org.team9140.frc2026;
 
 import org.team9140.frc2026.generated.TunerConstants;
+import org.team9140.frc2026.subsystems.Climber;
 import org.team9140.frc2026.subsystems.CommandSwerveDrivetrain;
+import org.team9140.frc2026.subsystems.Hopper;
+import org.team9140.frc2026.subsystems.Intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
+  private final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+  private final Climber climber = Climber.getInstance();
+  private final Hopper hopper = Hopper.getInstance();
+  private final Intake intake = Intake.getInstance();
 
   private final CommandXboxController controller = new CommandXboxController(0);
-  private final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
   private final SwerveTelemetry logger = new SwerveTelemetry(drivetrain, Constants.Drive.MAX_TELEOP_VELOCITY);
 
   public RobotContainer() {
@@ -22,10 +29,20 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
+    this.controller.rightBumper().whileTrue(this.intake.intake());
+    this.controller.leftBumper().whileTrue(this.intake.reverse().alongWith(this.hopper.unjam()));
+    this.controller.a().onTrue(new PrintCommand("start aiming"));
+    this.controller.x().onTrue(new PrintCommand("quit aiming"));
+    this.controller.rightTrigger().onTrue(new PrintCommand("throw")).onFalse(new PrintCommand("stop throw"));
+    this.controller.back().whileTrue(new PrintCommand("inch turret left"));
+    this.controller.start().whileTrue(new PrintCommand("inch turret right"));
+    this.controller.y().onTrue(this.climber.extend());
+    this.controller.b().onTrue(this.climber.retract());
+
     drivetrain.setDefaultCommand(
         drivetrain.teleopDrive(controller::getLeftX, controller::getLeftY,
             controller::getRightX).ignoringDisable(true));
-    
+
     this.drivetrain.registerTelemetry(logger::telemeterize);
   }
 
