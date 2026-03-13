f223574ced1ebed77c8702f7820ec0ace2e31aaf
ShauryaVatsal <vatshaurya@gmail.com>
2026-03-13 08:38:32 -0500

    added shooter transform

--- src/main/java/org/team9140/frc2026/helpers/AimAlign.java
+++ src/main/java/org/team9140/frc2026/helpers/AimAlign.java
@@ -7,6 +7,8 @@ import org.team9140.lib.Util;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -16,19 +18,21 @@ public class AimAlign {
     private static InterpolatingDoubleTreeMap lookupMotorSpeedFromDistance = new InterpolatingDoubleTreeMap();
     private static InterpolatingDoubleTreeMap lookupAirtimeFromDistance = new InterpolatingDoubleTreeMap();
 
-    static {
-        lookupMotorSpeedFromDistance.put(Double.valueOf(14.85), Double.valueOf(-0.11));
-        lookupMotorSpeedFromDistance.put(Double.valueOf(9.74), Double.valueOf(-0.119));
+    private static Transform2d SHOOTER_OFFSET = new Transform2d(0, 0, new Rotation2d()); //PLEASE UPDATE THIS VALUE
 
-        lookupAirtimeFromDistance.put(Double.valueOf(14.85), Double.valueOf(0.2));
-        lookupMotorSpeedFromDistance.put(Double.valueOf(9.74), Double.valueOf(-0.119));
+    static {
+        lookupMotorSpeedFromDistance.put(Double.valueOf(0), Double.valueOf(0));
+        lookupMotorSpeedFromDistance.put(Double.valueOf(5), Double.valueOf(5000));
+
+        lookupAirtimeFromDistance.put(Double.valueOf(0), Double.valueOf(0));
+        lookupAirtimeFromDistance.put(Double.valueOf(5), Double.valueOf(4));
     }
 
     public static Translation2d getEffectivePose(Pose2d robotPose, Translation2d goalPose, ChassisSpeeds robotSpeed) {
         Translation2d robotVelocity = new Translation2d(
                 robotSpeed.vxMetersPerSecond,
                 robotSpeed.vyMetersPerSecond);
-        Translation2d oldPose = goalPose.minus(robotPose.getTranslation());
+        Translation2d oldPose = goalPose.minus(robotPose.plus(SHOOTER_OFFSET).getTranslation());
         double airtime = lookupAirtimeFromDistance.get(oldPose.getNorm());
         Translation2d newPose = goalPose.minus(robotVelocity.times(airtime));
         while (oldPose.minus(newPose).getNorm() > 0.1) {
@@ -40,13 +44,14 @@ public class AimAlign {
     }
 
     public static double getRequiredSpeed(Pose2d robotPose, Translation2d effectivePose) {
-        return lookupMotorSpeedFromDistance.get(robotPose.getTranslation().minus(effectivePose).getNorm());
+        return lookupMotorSpeedFromDistance.get(robotPose.plus(SHOOTER_OFFSET).getTranslation().minus(effectivePose).getNorm());
     }
 
     public static double yawAngleToPos(Pose2d robotPose, Translation2d endPose) {
         return MathUtil.angleModulus(Math.atan2(
-                (endPose.getY() - robotPose.getY()), (endPose.getX() - robotPose.getX()))
-                - robotPose.getRotation().getRadians());
+                (endPose.getY() - robotPose.plus(SHOOTER_OFFSET).getY()), 
+                (endPose.getX() - robotPose.plus(SHOOTER_OFFSET).getX())
+            ) - robotPose.getRotation().getRadians());
     }
 
     public static Pose2d getZone(Pose2d robotPos) {
