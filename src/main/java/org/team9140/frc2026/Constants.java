e57267c768d37418cdf4d64b69017517ec0be034
ShauryaVatsal <vatshaurya@gmail.com>
2026-03-19 16:15:46 -0500

    changed forward and reversesoft limit thresholds to 200 and -200 degrees

--- src/main/java/org/team9140/frc2026/Constants.java
+++ src/main/java/org/team9140/frc2026/Constants.java
@@ -160,8 +160,8 @@ public class Constants {
         public static final double KD = 50;
 
         public static final double ADJUST_VOLTAGE = 2.0;
-        public static final double FORWARD_SOFT_LIMIT_THRESHOLD = 0.25; // rot
-        public static final double REVERSE_SOFT_LIMIT_THRESHOLD = -0.25; // rot
+        public static final double FORWARD_SOFT_LIMIT_THRESHOLD = 200.0 / 360.0; // rot
+        public static final double REVERSE_SOFT_LIMIT_THRESHOLD = -200.0 / 360.0; // rot
         public static final double GEAR_RATIO = 98.0 / 10.0 * 50.0 / 11.0;
 
         public static final double OVERTURN_LOOKAHEAD_TIME = 1.5;
