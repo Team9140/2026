3db597d6d0d6f31b79e5b0c55a8e4254e9c9578f
ShauryaVatsal <vatshaurya@gmail.com>
2026-04-04 17:03:33 -0500

    Applied shooterFeedbackConfigs

--- src/main/java/org/team9140/frc2026/subsystems/Shooter.java
+++ src/main/java/org/team9140/frc2026/subsystems/Shooter.java
@@ -220,7 +220,8 @@ public class Shooter extends SubsystemBase {
                 .withSlot0(shooterSlot0Configs)
                 .withMotorOutput(shooterMotorOutputConfigs)
                 .withTorqueCurrent(shooterTorqueCurrentConfigs)
-                .withCurrentLimits(shooterCurrentLimits);
+                .withCurrentLimits(shooterCurrentLimits)
+                .withFeedback(shooterFeedbackConfigs);
 
         // END FLYWHEEL CONFIG
 
